e248c91d50c9e62a51632b47620088f5033c715f
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-09-14 16:42:39 +0200

    created boid and boidmanager objects
    - boid has position and velocity
    - boid manager spawns several boids with random position/velocity

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
+    public void Initialize(float speed, Vector2 pos, Vector2 dir)
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.position = pos;
+        rb.velocity = dir * speed;
+
+    }
+}
---
+++ Assets/Scripts/BoidManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour
+{
+    [SerializeField] GameObject boidPrefab = null;
+    [SerializeField] private int numberOfBoids = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateBoids();
+    }
+
+    void GenerateBoids()
+    {
+        for(int i = 0; i < numberOfBoids; i++)
+        {
+            Boid boid = Instantiate(boidPrefab, Vector2.zero, Quaternion.identity).GetComponent<Boid>();
+
+            float rpx = Random.Range(-10f, 10f);
+            float rpy = Random.Range(-10f, 10f);
+            float rvx = Random.Range(-1f, 1f);
+            float rvy = Random.Range(-1f, 1f);
+            float rs = Random.Range(0f, 4f);
+
+            boid.Initialize(rs, new Vector2(rpx, rpy), new Vector2(rvx, rvy));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
